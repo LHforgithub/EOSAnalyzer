b4b539e29820857b2a81907da904b3cca14c4c38
2100314 <2100314@HZH1163N>
2025-11-12 14:48:33 +0800

    Update .gitignore to exclude dist/

--- EOSAnalyzer/GlobalSuppressions.cs
+++
@@ -1,9 +0,0 @@
-// This file is used by Code Analysis to maintain SuppressMessage attributes that are applied to this project. Project-level
-// suppressions either have no target or are given a specific target and scoped to a namespace, type, member, etc.
-
-using System.Diagnostics.CodeAnalysis;
-
-[assembly: SuppressMessage("MicrosoftCodeAnalysisPerformance", "RS1008:不要将每次编译的数据存储到诊断分析器的字段中。", Justification = "<挂起>", Scope = "type", Target = "~T:EOSAnalyzer.EOSAnalyzer")]
-[assembly: SuppressMessage("Style", "IDE0090:使用 \"new(...)\"", Justification = "<挂起>", Scope = "member", Target = "~T:EOSAnalyzer.EOSAnalyzer")]
-[assembly: SuppressMessage("Style", "IDE0090:使用 \"new(...)\"", Justification = "<挂起>", Scope = "member", Target = "~T:EOSAnalyzer.EOSAnalyzerDataBase")]
-[assembly: SuppressMessage("Style", "IDE0028:简化集合初始化", Justification = "<挂起>", Scope = "member", Target = "~T:EOSAnalyzer.EOSAnalyzer")]
